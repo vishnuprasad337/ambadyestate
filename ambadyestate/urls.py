7f0d344bf79eb6712bd7a120d28ed209903beeac
vishnu prasad <mvihnu673@gmail.com>
2026-07-14 18:40:28 +0530

    imggg

--- ambadyestate/urls.py
+++ ambadyestate/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', include('ambadyestate_app.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
