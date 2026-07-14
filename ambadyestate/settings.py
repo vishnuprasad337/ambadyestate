54a741a2e05d99df17bb50a219a24196a3337809
vishnu prasad <mvihnu673@gmail.com>
2026-07-14 18:17:00 +0530

    static

--- ambadyestate/settings.py
+++ ambadyestate/settings.py
@@ -119,6 +119,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
