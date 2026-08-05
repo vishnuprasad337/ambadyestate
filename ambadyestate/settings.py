ca3cec35f709017c441cf18a986fae378d78954b
vishnu prasad <mvihnu673@gmail.com>
2026-08-05 16:08:23 +0530

    Move uploaded media files into media directory

--- ambadyestate/settings.py
+++ ambadyestate/settings.py
@@ -123,6 +123,8 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
