69017987cd3a1752f7c7dc4da4182156b24598bb
vishnu prasad <mvihnu673@gmail.com>
2026-09-03 16:21:22 +0530

    sitemaps url

--- ambadyestate/urls.py
+++ ambadyestate/urls.py
@@ -18,8 +18,34 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+
+from ambadyestate_app.sitemaps import (
+    StaticViewSitemap,
+    RoomSitemap,
+    ActivitySitemap,
+    NearbyDestinationSitemap,
+    PackageSitemap,
+    BlogSitemap,
+)
+
+sitemaps = {
+    "static": StaticViewSitemap,
+    "rooms": RoomSitemap,
+    "activities": ActivitySitemap,
+    "nearby_destinations": NearbyDestinationSitemap,
+    "packages": PackageSitemap,
+    "blog": BlogSitemap,
+}
+
 urlpatterns = [
     #path('admin/', admin.site.urls),
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
     path('', include('ambadyestate_app.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
