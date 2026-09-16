2597ec9706c062c62b303b78feeb6a28d0947b46
vishnu prasad <mvihnu673@gmail.com>
2026-09-16 21:15:21 +0530

    truett

--- ambadyestate/urls.py
+++ ambadyestate/urls.py
@@ -81,8 +81,7 @@ urlpatterns = [
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 handler404 = "ambadyestate_app.views.page_404"
